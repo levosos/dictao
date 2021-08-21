98b2e7b35d546e853b46a962ae2bf9c4fb3b8579
Raz Rotenberg <raz.rotenberg@gmail.com>
2021-08-21 13:56:04 +0300

    Introducing 'Word' component
    - Encapsulating a word and its pronunciations

---
+++ src/components/Word.vue
@@ -0,0 +1,38 @@
+<template>
+  <span>
+    {{ word }}
+    <span class="pronunciation" v-if="pronounced && pronunciation"> {{ pronunciation }}</span>
+    <br v-if="newline" />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Word',
+  props: {
+    'word': {
+      type: String,
+    },
+    'pronunciation': {
+      type: String,
+      default: null,
+    },
+    'pronounced': {
+      type: Boolean,
+      default: true,
+    },
+    'newline': {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.pronunciation {
+  font-size: 70%;
+  font-weight: lighter;
+  opacity: 0.9;
+}
+</style>
